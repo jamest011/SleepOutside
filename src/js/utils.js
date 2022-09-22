2fd4182fd611567b27653feff4df08cc4d4295b1
Jhnstnj91 <Jhnstnj91@yahoo.com>
2022-09-22 11:57:21 -0400

    merging to main

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,9 +1,7 @@
 // wrapper for querySelector...returns matching element
-export function qs(selector, parent = document) {
-  return parent.querySelector(selector);
+export function qs(selector) {
+  return document.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
@@ -15,16 +13,15 @@ export function setLocalStorage(key, data) {
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
-  qs(selector).addEventListener("touchend", (event) => {
+  qs(selector).addEventListener('touchend', (event) => {
     event.preventDefault();
     callback();
   });
-  qs(selector).addEventListener("click", callback);
+  qs(selector).addEventListener('click', callback);
 }
 
-//get the URL parameters
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get('product')
+  return urlParams.get(param);
 }
