02245da0dae747f98d7f149af26eaa7ea2fda855
RhodaMabs <mab21004@byui.edu>
2022-10-06 16:14:13 +0200

    changes by npm run build

--- build/js/cart.js
+++ build/js/cart.js
@@ -1,14 +1,14 @@
-function getLocalStorage(a){return JSON.parse(localStorage.getItem(a))}function getCartContents(){let a="";const r=getLocalStorage("so-cart"),c=r.map(t=>renderCartItem(t));document.querySelector(".product-list").innerHTML=c.join("")}function renderCartItem(a){const r=`<li class="cart-card divider">
+const shopping_cart=document.querySelector(".cart"),cart_buttons=document.querySelectorAll(".addToCart");function getLocalStorage(t){return JSON.parse(localStorage.getItem(t))}function getCartContents(){let t="";const r=getLocalStorage("so-cart"),c=r.map(a=>renderCartItem(a));document.querySelector(".product-list").innerHTML=c.join("")}function renderCartItem(t){const r=`<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${a.Image}"
-      alt="${a.Name}"
+      src="${t.Image}"
+      alt="${t.Name}"
     />
   </a>
   <a href="#">
-    <h2 class="card__name">${a.Name}</h2>
+    <h2 class="card__name">${t.Name}</h2>
   </a>
-  <p class="cart-card__color">${a.Colors[0].ColorName}</p>
+  <p class="cart-card__color">${t.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${a.FinalPrice}</p>
-</li>`;return console.log(r),r}getCartContents();
+  <p class="cart-card__price">$${t.FinalPrice}</p>
+</li>`;return console.log(r),r}getCartContents();for(cart_buttons of cart_buttons)cart_buttons.onclick=t=>{let r=Number(shopping_cart.getAttribute("data-product-count"))||0;shopping_cart.setAttribute("data-product-count",r+1)};
