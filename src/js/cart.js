2000e5fd4d187da901fb2105ef3a15c3a93e2d95
RhodaMabs <mab21004@byui.edu>
2022-10-07 09:38:42 +0200

    replaces

--- src/js/cart.js
+++ src/js/cart.js
@@ -8,5 +8,5 @@ cart.init();
 //check to see if there is anything in the cart...
 if (cart.total > 0) {
   // show our checkout button and total if there are items in the cart.
-  document.querySelector('.list-footer').classList.remove('hide');
+  document.querySelector('.cart-footer').classList.remove('hide');
 }
